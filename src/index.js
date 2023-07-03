ee8069d228993afcbd193c2f7e18a901fc1f5dba
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-07-03 19:56:42 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BreadCrumb } from './components/Teste/BreadCrumb';
+import EnderecoForm from './components/EnderecoForm/EnderecoForm';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BreadCrumb/>
+    <EnderecoForm/>
   </React.StrictMode>
 );
